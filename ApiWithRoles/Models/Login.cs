154781976f06294d73847e2f3507fb7129bc2a1c
MazenSokkar <mazensokar.priv@gmail.com>
2025-04-22 18:53:12 +0200

    Add project files.

---
+++ ApiWithRoles/Models/Login.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ApiWithRoles.Models
+{
+    public class Login
+    {
+        public String Username { get; set; } = string.Empty;
+        public String Password { get; set; } = string.Empty;
+    }
+}
